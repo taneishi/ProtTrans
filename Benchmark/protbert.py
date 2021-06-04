285c6b1117263913cec8c7c306439b80ab0c0a83
taneishi <taneishi@users.noreply.github.com>
2021-06-04 10:18:02 +0900

    imported CLI protbert benchmark.

---
+++ Benchmark/protbert.py
@@ -0,0 +1,99 @@
+# <h3> Benchmark ProtBert Model using GPU or CPU <h3>
+
+# <b>1. Load necessry libraries including huggingface transformers<b>
+
+# !pip install -q transformers
+
+import torch
+from transformers import BertModel
+import time
+from datetime import timedelta
+import requests
+import argparse
+import os
+from tqdm.auto import tqdm
+
+def main(args):
+    os.makedirs(args.modelFolderPath, exist_ok=True)
+
+    def download_file(url, filename):
+      response = requests.get(url, stream=True)
+      with tqdm.wrapattr(open(filename, "wb"), "write", miniters=1,
+                        total=int(response.headers.get('content-length', 0)),
+                        desc=filename) as fout:
+          for chunk in response.iter_content(chunk_size=4096):
+              fout.write(chunk)
+
+    if not os.path.exists(args.modelFilePath):
+        download_file(args.modelUrl, args.modelFilePath)
+
+    if not os.path.exists(args.configFilePath):
+        download_file(args.configUrl, args.configFilePath)
+
+    if not os.path.exists(args.vocabFilePath):
+        download_file(args.vocabUrl, args.vocabFilePath)
+
+    # <b>4. Load ProtBert Model<b>
+
+    model = BertModel.from_pretrained(args.modelFolderPath)
+
+    # <b>5. Load the model into the GPU if avilabile and switch to inference mode<b>
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    model = model.to(device)
+    model = model.eval()
+
+    # <b>6. Benchmark Configuration<b>
+
+    min_batch_size = 8
+    max_batch_size = 32
+    inc_batch_size = 8
+
+    min_sequence_length = 64
+    max_sequence_length = 512
+    inc_sequence_length = 64
+
+    iterations = 10
+
+    # <b>7. Start Benchmarking<b>
+
+    device_name = torch.cuda.get_device_name(device.index) if device.type == 'cuda' else 'CPU'
+
+    with torch.no_grad():
+        print((' Benchmarking using ' + device_name + ' ').center(80, '*'))
+        print(' Start '.center(80, '*'))
+        for sequence_length in range(min_sequence_length,max_sequence_length+1,inc_sequence_length):
+            for batch_size in range(min_batch_size, max_batch_size+1, inc_batch_size):
+                start = time.time()
+                for i in range(iterations):
+                    input_ids = torch.randint(1, 20, (batch_size,sequence_length)).to(device)
+                    results = model(input_ids)[0].cpu().numpy()
+                end = time.time()
+                ms_per_protein = (end-start) / (iterations*batch_size)
+                print('Sequence Length: %4d \t Batch Size: %4d \t Ms per protein %4.2f' %(sequence_length, batch_size, ms_per_protein))
+            print(' Done '.center(80, '*'))
+        print(' Finished '.center(80, '*'))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # <b>2. Set the url location of ProtBert and the vocabulary file<b>
+    parser.add_argument('--modelUrl', default='https://www.dropbox.com/s/dm3m1o0tsv9terq/pytorch_model.bin?dl=1')
+    parser.add_argument('--configUrl', default='https://www.dropbox.com/s/d3yw7v4tvi5f4sk/bert_config.json?dl=1')
+    parser.add_argument('--vocabUrl', default='https://www.dropbox.com/s/jvrleji50ql5m5i/vocab.txt?dl=1')
+
+    # <b>3. Download ProtBert models and vocabulary files<b>
+    parser.add_argument('--downloadFolderPath', default='models/ProtBert/')
+    parser.add_argument('--modelFolderPath', default='models/ProtBert/')
+    parser.add_argument('--modelFile', default='pytorch_model.bin')
+    parser.add_argument('--configFile', default='config.json')
+    parser.add_argument('--vocabFile', default='vocab.txt')
+
+    args = parser.parse_args([])
+    args.modelFilePath = os.path.join(args.modelFolderPath, args.modelFile)
+    args.configFilePath = os.path.join(args.modelFolderPath, args.configFile)
+    args.vocabFilePath = os.path.join(args.modelFolderPath, args.vocabFile)
+    print(vars(args))
+
+    main(args)
